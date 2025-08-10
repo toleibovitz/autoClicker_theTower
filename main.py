da30afe3eb22c6d98b29327857e4ed5fe72e4dc0
toleibovitz <toleibovitz@gmail.com>
2025-08-09 23:04:01 -0500

    adding logic for retry button click

--- main.py
+++ main.py
@@ -12,20 +12,20 @@ import numpy as np
 
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-def enum_window_titles():
-    windows = []
-    def callback(hwnd, extra):
-        if win32gui.IsWindowVisible(hwnd):
-            title = win32gui.GetWindowText(hwnd)
-            if title:
-                windows.append((hwnd, title))
-    win32gui.EnumWindows(callback, None)
-    return windows
+# def enum_window_titles():
+#     windows = []
+#     def callback(hwnd, extra):
+#         if win32gui.IsWindowVisible(hwnd):
+#             title = win32gui.GetWindowText(hwnd)
+#             if title:
+#                 windows.append((hwnd, title))
+#     win32gui.EnumWindows(callback, None)
+#     return windows
 
-def get_monitor_for_window(hwnd):
-    hmonitor = win32api.MonitorFromWindow(hwnd, win32con.MONITOR_DEFAULTTONEAREST)
-    info = win32api.GetMonitorInfo(hmonitor)
-    return info
+# def get_monitor_for_window(hwnd):
+#     hmonitor = win32api.MonitorFromWindow(hwnd, win32con.MONITOR_DEFAULTTONEAREST)
+#     info = win32api.GetMonitorInfo(hmonitor)
+#     return info
 
 # this get the middle of the ad gem button
 def get_middle_position_ad_gem():
@@ -36,8 +36,8 @@ def get_middle_position_ad_gem():
         
         left, top, right, bottom = rect
         width = right - left
-        x_ad_gem = left + (width * .15)
-        y_ad_gem = top + (bottom - top) * .44
+        x_ad_gem = round(left + (width * .15))
+        y_ad_gem = round(top + (bottom - top) * .44)
 
         ad_gem_coords = (x_ad_gem, y_ad_gem)
         return ad_gem_coords
@@ -75,6 +75,11 @@ def get_middle_position_retry():
         
         left, top, right, bottom = rect
         width = right - left
+        x_retry = round(left + (width * .25))
+        y_retry = round(top + (bottom - top) * .74)
+
+        retry_button_middle_coords = (x_retry, y_retry)
+        return retry_button_middle_coords
 
 
 def get_retry_button_area():
@@ -86,19 +91,19 @@ def get_retry_button_area():
         left, top, right, bottom = rect
         width = right - left
 
-        left_x = round(left + (width * .03))
-        right_x = round(left + (width * .23))
-
-        top_y = round(top + (bottom - top) * .40)
-        bottom_y = round(top + (bottom - top) * .47) 
+        left_x = round(left + (width * .09))
+        top_y = round(top + (bottom - top) * .71)
+        
+        
+        right_x = round(left + (width * .45))
+        bottom_y = round(top + (bottom - top) * .77) 
 
         return (left_x, top_y, right_x, bottom_y)
 
 
 
 
-def capture_region_and_check():
-    coords = get_ad_gem_area()
+def capture_region_and_check(coords, text, text_to_find):
     if not coords:
         print("Window not found.")
         return False
@@ -110,33 +115,51 @@ def capture_region_and_check():
     with mss.mss() as sct:
         monitor = {"top": top, "left": left, "width": width, "height": height}
         img = np.array(sct.grab(monitor))
-
+        
     
     # gray = cv2.cvtColor(img, cv2.COLOR_BGRA2GRAY)
 
     
-    text = pytesseract.image_to_string(img)
+    text_read = pytesseract.image_to_string(img)
     
-    print("Detected text:", text.strip())
+    if text in text_read.strip():
+        print(f"{text_to_find} Found")
+        return True
+    else:
+        print(f"{text_to_find} NOT Found")
+        return False
 
-    return "CLAIM" in text  
+     
 
 def main():
-    coords = get_retry_button_area()
+    retry_text = "RETRY"
+    ad_gem_text = "CLAIM"
+    retry_clicked = 0
+    ad_gem_clicked = 0
 
-    pyautogui.moveTo()
-    # while True:
-    #     if capture_region_and_check():
-    #         print("Found Target Text")
-    #         coords = get_middle_position_ad_gem()
-    #         pyautogui.moveTo(coords)
-    #         pyautogui.click()
 
-    #     else:
-    #         print("Nothing Found")
-    #     time.sleep(60)
+    while True:
+        retry_coords = get_retry_button_area()
+        is_retry_shown = capture_region_and_check(retry_coords, retry_text, "RETRY" )
+        
+        if is_retry_shown:
+            retry_button_middle_coords = get_middle_position_retry()
+            pyautogui.moveTo(retry_button_middle_coords)
+            pyautogui.click()
+            retry_clicked += 1
+            print(f"Retry Clicked {retry_clicked} time(s)")
+        elif capture_region_and_check(get_ad_gem_area(), ad_gem_text, "AD GEM CLAIM"):
+            ad_gem_middle = get_middle_position_ad_gem()
+            pyautogui.moveTo(ad_gem_middle)
+            pyautogui.click()
+            ad_gem_clicked += 1
+            print(f"Ad Gem Clicked {ad_gem_clicked} time(s)")
+        else:
+            print("No Retry or Ad Gem found. Trying again in 120 seconds...")
+        time.sleep(120)
+
     
-
+    
 
 
 if __name__ == "__main__":
